ba970116ceef5779e6e863bb223166e0361e9c80
Torfa <alextison@icloud.com>
2022-12-04 14:33:57 +0100

    Day4 part2

--- day4/src/file.ts
+++ day4/src/file.ts
@@ -44,4 +44,45 @@ function getTotalContains() {
   return totalContain;
 }
 
-console.log(getTotalContains());
+function checkIfOverlap(
+  number1: number,
+  number2: number,
+  range1: number,
+  range2: number
+) {
+  if (
+    (number1 >= range1 && number1 <= range2) ||
+    (number2 >= range1 && number2 <= range2)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function isOverlap(first: string, second: string) {
+  let firstB = +first.split("-")[0];
+  let firstE = +first.split("-")[1];
+  let secondB = +second.split("-")[0];
+  let secondE = +second.split("-")[1];
+  if (
+    checkIfOverlap(firstB, firstE, secondB, secondE) ||
+    checkIfOverlap(secondB, secondE, firstB, firstE)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function getTotalOverlap() {
+  const allFileContents = fs4.readFileSync("input.txt", "utf-8");
+  allFileContents.split(/\r?\n/).forEach((line) => {
+    let first = line.split(",")[0];
+    let second = line.split(",")[1];
+    if (isOverlap(first, second)) {
+      totalContain++;
+    }
+  });
+  return totalContain;
+}
+
+console.log(getTotalOverlap());
